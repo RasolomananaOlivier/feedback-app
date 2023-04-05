e1b2f94eebe00e8928a9c0be942c6356d54a146b
Rasolomanana Herimanitra Olivier <herimanitraolivierr@gmail.com>
2023-04-05 09:29:30 +0300

    feat(App.svelte): add FeedbackList component and deleteFeedback function The FeedbackList component is added to the App component to display a list of feedbacks. The feedbacks array is initialized with two test feedbacks. The deleteFeedback function is added to handle the delete-feedback event emitted by the FeedbackList component. When a feedback is deleted, the feedbacks array is updated to remove the deleted feedback.

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,27 @@
 <script lang="ts">
-  let a = 1;
-  let b = 1;
+  import FeedbackList from "./lib/components/FeedbackList.svelte";
 
+  let feedbacks: Feedback[] = [
+    {
+      id: "1",
+      comment: "This is a test feedback",
+      rating: 5,
+      createdAt: new Date("2023-01-01"),
+    },
+    {
+      id: "2",
+      comment: "This is a test feedback",
+      rating: 8,
+      createdAt: new Date("2023-04-05"),
+    },
+  ];
+
+  function deleteFeedback(e) {
+    const feedbackId = e.detail.id;
+    feedbacks = feedbacks.filter((feedback) => feedback.id !== feedbackId);
+  }
 </script>
 
 <main>
-  <input type="number" bind:value={a}>
-  <input type="number" bind:value={b}>
-
-  {a} + {b} = {a + b}
+  <FeedbackList {feedbacks} on:delete-feedback={deleteFeedback} />
 </main>
-
