6401328c08391b598ebecfbf760a9a188225863e
Rasolomanana Herimanitra Olivier <herimanitraolivierr@gmail.com>
2023-04-05 08:15:19 +0300

    chore: set up the app

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  let a = 1;
+  let b = 1;
+
+</script>
+
+<main>
+  <input type="number" bind:value={a}>
+  <input type="number" bind:value={b}>
+
+  {a} + {b} = {a + b}
+</main>
+
